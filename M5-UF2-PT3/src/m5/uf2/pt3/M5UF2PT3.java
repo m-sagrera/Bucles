59b221c7b20b00a53d5d6a4ebbd499f55db5e140
msagrera.daw1r <msagrera.daw1r@embat-17>
2022-05-06 15:46:17 +0200

    UwU2

--- M5-UF2-PT3/src/m5/uf2/pt3/M5UF2PT3.java
+++ M5-UF2-PT3/src/m5/uf2/pt3/M5UF2PT3.java
@@ -7,6 +7,7 @@ public class M5UF2PT3 {
         int num = 2000;
         int cont = 0;
         int cont2 = 0;
+        
         for (int i = 0; i < num; i++) {
             while (cont != 2000) {
                 for (int j = 0; j < num; j++) {
